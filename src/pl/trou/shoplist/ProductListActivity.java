d7cba14246d6c5dbc36806e9abe9bc452fec0388
Michal Kulesza <mickul@gmail.com>
2014-10-27 22:17:17 +0100

    initial version with 2 activites

---
+++ src/pl/trou/shoplist/ProductListActivity.java
@@ -0,0 +1,15 @@
+package pl.trou.shoplist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ProductListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_product_list);
+	}
+}
